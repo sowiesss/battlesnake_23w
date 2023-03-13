65a2843974e6fa27212d4190085161953d689950
sss <10131931+sowiesss@users.noreply.github.com>
2023-03-13 15:50:18 -0400

    set up brain

---
+++ snake_brain.py
@@ -0,0 +1,22 @@
+def next_head(move:str, old_head):
+    if move == "up":
+        new_head = {"x": old_head["x"], "y": old_head["y"] + 1}
+    if move == "down":
+        new_head = {"x": old_head["x"], "y": old_head["y"] - 1}
+    if move == "left":
+        new_head = {"x": old_head["x"] - 1, "y": old_head["y"]}
+    if move == "right":
+        new_head = {"x": old_head["x"] + 1, "y": old_head["y"]}
+    return new_head
+
+
+def close_to(my_snake, opponent, dis):
+    if abs(my_snake["y"]-opponent["y"]) + abs(my_snake["x"] - opponent["x"]) == dis:
+        return True
+    else:
+        return False
+
+
+
+
+       
